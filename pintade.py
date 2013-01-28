933a6a2d4caa94747bf1e5f8382d6df574807dbb
ftorregrosa <florent.torregrosa@centrale-marseille.fr>
2013-01-28 11:05:41 +0100

    adding initial population

--- pintade.py
+++ pintade.py
@@ -43,29 +43,12 @@ tab_pondeuse = tps_vie*[0]
 tab_standard = tps_maturation_standard*[0]
 tab_label_rouge = tps_maturation_label_rouge*[0]
 
-#import glpk            # Import the GLPK module
-#
-#lp_capacite = glpk.LPX()        # Create empty problem instance
-#lp_capacite.name = 'sample'     # Assign symbolic name to problem
-#lp_capacite.obj.maximize = True # Set this as a maximization problem
-#lp_capacite.rows.add(3)         # Append three rows to this instance
-#for r in lp_capacite.rows:      # Iterate over all rows
-#	r.name = chr(ord('p')+r.index) # Name them p, q, and r
-#lp_capacite.rows[0].bounds = None, 100.0  # Set bound -inf < p <= 100
-#lp_capacite.rows[1].bounds = None, 600.0  # Set bound -inf < q <= 600
-#lp_capacite.rows[2].bounds = None, 300.0  # Set bound -inf < r <= 300
-#lp_capacite.cols.add(3)         # Append three columns to this instance
-#for c in lp_capacite.cols:      # Iterate over all columns
-#	c.name = 'x%d' % c.index # Name them x0, x1, and x2
-#	c.bounds = 0.0, None     # Set bound 0 <= xi < inf
-#lp_capacite.obj[:] = [ 10.0, 6.0, 4.0 ]   # Set objective coefficients
-#lp_capacite.matrix = [ 1.0, 1.0, 1.0,     # Set nonzero entries of the
-#             10.0, 4.0, 5.0,     #   constraint matrix.  (In this
-#              2.0, 2.0, 6.0 ]    #   case, all are non-zero.)
-#lp_capacite.simplex()           # Solve this LP with the simplex method
-#print 'Z = %g;' % lp_capacite.obj.value,  # Retrieve and print obj func value
-#print '; '.join('%s = %g' % (c.name, c.primal) for c in lp_capacite.cols)
-#                       # Print struct variable names and primal values
+def pondeuse_initale():
+	capacite_pondeuse = capacite_pintade()[3]
+	for index in range(tab_pondeuse):
+		if capacite_pondeuse >= 0:
+			capacite_pondeuse -= 10
+			tab_pondeuse[index] = 10
 
 def cequilsepasseparmois():
 	#gérér les oeufs
